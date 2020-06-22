fc94ddbb12d8f3812e88e40121567851d60eecc4
Ryan Camp <campryan@comcast.net>
2020-06-22 14:42:47 -0600

    Create API server with endpoints to list all papers and footnotes

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Publications';
+
+app.get('/', (req, res) => {
+  res.send('Hello, Publications!');
+});
+
+app.get('/api/v1/papers', (req, res) => {
+  database('papers').select()
+  .then(papers => res.status(200).json(papers))
+  .catch(error => res.status(500).json({ error }));
+});
+
+app.get('/api/v1/footnotes', (req, res) => {
+  database('footnotes').select()
+  .then(footnotes => res.status(200).json(footnotes))
+  .catch(error => res.status(500).json({ error }));
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+});
+debugger;
